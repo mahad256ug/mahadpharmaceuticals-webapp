67cfab7562c0fefcb93b174ee05990b12d878871
ukasha070 <ukashakats1@gmail.com>
2025-11-29 02:57:56 +0300

    resolved

--- frontend/src/app/drugs/page.tsx
+++ frontend/src/app/drugs/page.tsx
@@ -5,7 +5,7 @@ import { Metadata } from "next";
 import DrugsPageContent from "./_components/page-content";
 
 export const metadata: Metadata = {
-  title: "find and buy abortion, contraceptives ED in UAE",
+  title: "find and buy abortion pills near me, contraceptives ED in UAE",
   keywords:
     " genuine abortion pills and contraceptives in the UAE . Trusted medical abortion information, privacy-focused service, and access to essential reproductive health products.",
   description:
